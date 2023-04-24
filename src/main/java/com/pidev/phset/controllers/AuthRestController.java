a7145ce81b7ffcccdf7f990f4fbf9da5fbb40267
Ghofrane <BenSoltane.Ghofrane@esprit.tn>
2023-04-24 14:50:29 +0100

    add cors conf to communicate with front end

--- src/main/java/com/pidev/phset/controllers/AuthRestController.java
+++ src/main/java/com/pidev/phset/controllers/AuthRestController.java
@@ -1,24 +1,11 @@
 package com.pidev.phset.controllers;
 
-import com.pidev.phset.entities.Role;
 import com.pidev.phset.security.dto.TokenDto;
 import com.pidev.phset.services.AuthenticationService;
 import lombok.extern.log4j.Log4j2;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import javax.annotation.security.RolesAllowed;
-import java.io.File;
-import java.io.IOException;
 
 
 @RestController
--- src/main/java/com/pidev/phset/security/SecurityConfig.java
+++ src/main/java/com/pidev/phset/security/SecurityConfig.java
@@ -13,19 +13,10 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 
 @Configuration
@@ -97,4 +88,5 @@ public class SecurityConfig {
 
         return http.build();
     }
+
 }
